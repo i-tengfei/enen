ce857be536b327f64410fbd22598ce5aabd9bab8
mrtone <mrtone@malubei.com>
2014-02-27 10:14:43 +0800

    fix gruntfile bug

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,7 +28,7 @@ module.exports = function( grunt ) {
         uglifyTask = {},
         lessTask = {
             options: {
-                compress: true
+                cleancss: true
             }
         };
     
