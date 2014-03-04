47f91e3a5ce4597a983dbed6645f1942d7332e1c
mrtone <mrtone@malubei.com>
2014-03-04 15:58:37 +0800

    for heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var fs = require( 'fs' ),
     exec = require('child_process').exec;
+    
 // for openshift and heroku
 if( process.env.DATABASE_PRODUCTION && process.env.DATABASE_DEVELOPMENT ){
 
@@ -8,23 +9,7 @@ if( process.env.DATABASE_PRODUCTION && process.env.DATABASE_DEVELOPMENT ){
     fs.writeFileSync( './server/database.production', process.env.DATABASE_PRODUCTION );
     fs.writeFileSync( './server/database.development', process.env.DATABASE_DEVELOPMENT );
 
-    exec( './node_modules/bower/bin/bower install', { cwd: './' }, function ( e ) {
-
-        e && console.log( e );
-
-        exec( './node_modules/grunt-cli/bin/grunt', { cwd: './' }, function ( e ) {
-
-            e && console.log( e );
-            start( );
-
-        } );
-
-    } );
-}else{
-    start( );
 }
 
-function start( ){
-    process.env.NODE_ENV = 'production';
-    require( './server/app' );
-}
+process.env.NODE_ENV = 'production';
+require( './server/app' );
