806a210a36ddd36c3e2daca92ba6d3fa927a6360
mrtone <mrtone@malubei.com>
2014-03-04 16:42:07 +0800

    for heroku

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ var fs = require( 'fs' ),
 if( process.env.DATABASE_PRODUCTION && process.env.DATABASE_DEVELOPMENT ){
 
     process.env.ENEN_PORT = process.env.PORT;
-    process.env.ENEN_IP = process.env.IP;
 
     fs.writeFileSync( './server/database.production', process.env.DATABASE_PRODUCTION );
     fs.writeFileSync( './server/database.development', process.env.DATABASE_DEVELOPMENT );
