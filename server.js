decf4d87adab41b8a194ee254c58a1bfa3690c74
mrtone <mrtone@malubei.com>
2014-03-04 15:36:49 +0800

    start

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ if( process.env.DATABASE_PRODUCTION && process.env.DATABASE_DEVELOPMENT ){
     fs.writeFileSync( './server/database.production', process.env.DATABASE_PRODUCTION );
     fs.writeFileSync( './server/database.development', process.env.DATABASE_DEVELOPMENT );
 
-    exec( 'HOME=/tmp ./node_modules/bower/bin/bower install', { cwd: './' }, function ( e ) {
+    exec( './node_modules/bower/bin/bower install', { cwd: './' }, function ( e ) {
 
         e && console.log( e );
 
