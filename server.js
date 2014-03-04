65dd3d670941a7a7147f24ce873b92f4bd489da7
mrtone <mrtone@malubei.com>
2014-03-04 16:39:13 +0800

    for heroku

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var fs = require( 'fs' ),
 // for openshift and heroku
 if( process.env.DATABASE_PRODUCTION && process.env.DATABASE_DEVELOPMENT ){
 
-    process.env.ENEN_PORT = process.env.PORT || 5000;
+    process.env.ENEN_PORT = process.env.PORT;
+    process.env.ENEN_IP = process.env.IP;
 
     fs.writeFileSync( './server/database.production', process.env.DATABASE_PRODUCTION );
     fs.writeFileSync( './server/database.development', process.env.DATABASE_DEVELOPMENT );
